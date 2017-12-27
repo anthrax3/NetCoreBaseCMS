9386ae98b177d79bbd4a0338eefa06a518c04eea
Janak Shrestha <old_ink@hotmail.com>
2017-12-27 14:45:05 +0545

    Base project added

---
+++ NetCoreBaseCMS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreBaseCMS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
